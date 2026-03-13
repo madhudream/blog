f67d56759ae42ab1cb77756fc656323a5d5eae4f
dev-vatuka <dev@vatukalabs.in>
2026-03-13 11:41:37 -0500

    updated astro to fix cloudflare build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
-
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://madhudream.dev',
+  output: 'static',
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [mdx()]
 });
